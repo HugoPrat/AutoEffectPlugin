9bbb2150ac23339bdf10050f3f3a6602f4113d2a
Hugo PRAT <you@example.com>
2022-06-01 17:48:09 +0200

    REMOVE JUCER file, setting project by CMake, allowing to add GoogleUnit test, adding VERSION file

--- Source/EffectProcessors.h
+++ Source/EffectProcessors.h
@@ -10,7 +10,8 @@
 
 #pragma once
 
-#include <JuceHeader.h>
+#include <juce_audio_processors/juce_audio_processors.h>
+#include <juce_dsp/juce_dsp.h>
 
 //==============================================================================
 class ProcessorBase  : public juce::AudioProcessor
